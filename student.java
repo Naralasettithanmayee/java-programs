66ace998d5899517ac92a04371a664dcaf82ccca
Naralasettithanmayee <71427794+Naralasettithanmayee@users.noreply.github.com>
2021-07-02 14:39:58 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,18 @@
+class Student
+{
+int sid;
+String sname;
+float sfees;
+}
+class Student1
+{
+public static void main(String args[])
+{
+Student s1=new Student();
+s1.sid=12;
+s1.sname="chan";
+s1.sfees=34.4f;
+System.out.println(s1.sid+" "+s1.sname +" "+s1.sfees);
+}
+}
+
