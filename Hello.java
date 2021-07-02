b807c12707177eef5dc380b4a5a6a90d09a7823d
Naralasettithanmayee <71427794+Naralasettithanmayee@users.noreply.github.com>
2021-07-02 14:35:49 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.lang.*;
+import java.applet.*;
+public class Hello extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("hai",100,100);
+}
+}
+/*<Applet code="Hello.class" width="100" height="100">*/
