0177f2feb3e8b9d2012c127fec4e16619c018377
Naralasettithanmayee <71427794+Naralasettithanmayee@users.noreply.github.com>
2021-07-02 15:00:25 +0530

    Add files via upload

---
+++ files.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class files
+{
+public static void main(String args[])
+{
+File obj = newFile(" "/java/system" ");
+System.out.print(obj.canWrite());
+System.out.print(" "  " " + obj.canRead());
+}
+}
